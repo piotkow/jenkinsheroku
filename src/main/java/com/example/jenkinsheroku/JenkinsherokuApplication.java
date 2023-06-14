a069136c73528912f4e4ce167c8c5338e462b1a5
Piotr Kowalczyk <piotkow133@student.polsl.pl>
2023-06-14 22:30:27 +0200

    Initial commit

---
+++ src/main/java/com/example/jenkinsheroku/JenkinsherokuApplication.java
@@ -0,0 +1,21 @@
+package com.example.jenkinsheroku;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class JenkinsherokuApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JenkinsherokuApplication.class, args);
+    }
+
+    @GetMapping
+    public String get(){
+        return "Hello Jenkins";
+    }
+
+}
